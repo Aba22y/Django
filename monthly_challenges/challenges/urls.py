781caf23be1a8289f8ad5ed1eb6cb95b5f7e32fc
Ahmed Al Abassy <z5312664@ad.unsw.edu.au>
2024-05-22 21:28:03 +1000

    route/url redirection and dynamic django tools

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<str:month>", views.monthly_challenge)
+    path("<int:month>", views.monthly_challenge_numbers),
+    path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.urls import reverse
+
+months = {
+    "jan": "run",
+    "feb": "jump",
+    "mar": "swim",
+    "apr": "walk",
+    "may": "code",
+    "jun": "sleep",
+    "jul": "reflect",
+    "aug": "eat",
+    "sep": "sing",
+    "oct": "lament",
+    "nov": "django",
+    "dec": "gym"
+}
 
 # Create your views here.
 
+
+def monthly_challenge_numbers(req, month):
+    monthList = list(months.keys())
+    if month > len(monthList) or month < 1:
+        return HttpResponseNotFound("Invalid month")
+    redirect = monthList[month-1]
+    #this is a dynamic way to generate the url instead of hard coding /challenges/
+    redirectPath = reverse("month-challenge", args=[redirect])
+    return HttpResponseRedirect(redirectPath)
+
 def monthly_challenge(req, month):
-    challenge_text = None
-    if month == "jan":
-        challenge_text = "first day of da month"
-    elif month == "feb":
-        challenge_text = "second day of da month"
-    else:
-        return HttpResponseNotFound("This month is not supported")
-    return HttpResponse(challenge_text)
+    try:
+        challenge = months[month]
+        return HttpResponse(challenge)
+    except:
+        return HttpResponseNotFound("Type in a three letter month")
